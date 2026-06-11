03730c52f5a87a4a2203160920f015bae2a532ed
Maggus-93 <lucayi93@gmail.com>
2026-06-11 17:46:11 +0200

    Update server.py

--- server.py
+++ server.py
@@ -63,7 +63,7 @@ def products():
             {
                 "name":     "PC Optimizer",
                 "desc":     "Optimiert deinen PC für maximale Performance.",
-                "img":      "https://i.imgur.com/IL8MCP0.png",
+                "img":      "https://raw.githubusercontent.com/Maggus-93/evo-server/main/myoptimizer.png",
                 "file_url": "https://drive.google.com/file/d/1csjNprYTRFb4Qku9Em-dwq7UXqAjk32K/view?usp=sharing",
                 "filename": "optimizer.exe"
             }
